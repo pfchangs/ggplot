093371ed4fc659341d49281eff0f45c42ada41ea
pfchang <paulchanag1694@gmail.com>
2017-05-14 19:31:29 -0400

    ggplot assignment 1

---
+++ ggplotassignment1.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+
+titanic <- read.csv(file = 'titanic_clean.csv', stringsAsFactors = FALSE)
+
+
+# Check out the structure of titanic
+str(titanic)
+
+# Use ggplot() for the first instruction
+ggplot(titanic, aes(x = factor(pclass), fill = factor(sex))) + 
+  geom_bar(position = "dodge")
+
+# Use ggplot() for the second instruction
+ggplot(titanic, aes(x = factor(pclass), fill = factor(sex))) + 
+  geom_bar(position = "dodge") + 
+  facet_grid(". ~ Survived")
+
+# Position jitter (use below)
+posn.j <- position_jitter(0.5, 0)
+
+# Use ggplot() for the last instruction
+ggplot(titanic, aes(x = factor(pclass), y = age, col = factor(sex))) + 
+  geom_jitter(size = 3, alpha = 0.5, position = posn.j) + 
+  facet_grid(. ~ survived)
+
